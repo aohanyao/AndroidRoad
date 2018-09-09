f17a751a4d9eea64c39a3f445a657811026df5a3
江俊超 <928692385@qq.com>
2018-09-09 14:29:59 +0800

    finish blog
    finish blog

--- ColdStart/app/src/main/java/com/jc/road/SplashActivity.java
+++ ColdStart/app/src/main/java/com/jc/road/SplashActivity.java
@@ -17,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_splash);
-
+//        StatusBarUtil.setTranslucentForImageView(this, null);
 
         new Handler().postDelayed(new Runnable() {
             @Override
