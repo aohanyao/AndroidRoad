3f55cd955c704ad05c530d3b3801c15feeb2280d
江俊超 <928692385@qq.com>
2018-09-25 22:41:41 +0800

    add UploadBackstageDemo
    add UploadBackstageDemo

---
+++ UploadBackstageDemo/src/main/kotlin/com/jc/upload/UploadBackstageDemo/UploadBackstageDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.jc.upload.UploadBackstageDemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UploadBackstageDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<UploadBackstageDemoApplication>(*args)
+}
---
+++ UploadBackstageDemo/src/test/kotlin/com/jc/upload/UploadBackstageDemo/UploadBackstageDemoApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.jc.upload.UploadBackstageDemo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class UploadBackstageDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
